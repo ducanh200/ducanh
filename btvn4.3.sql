1fce2fb07a48e5e701c056329729eacefa9c9636
ducanh200 <112382276+ducanh200@users.noreply.github.com>
2022-11-06 14:29:56 +0700

    Delete btvn4.3.sql

--- btvn4.3.sql
+++
@@ -1,9 +0,0 @@
-﻿insert into khachhang(ten,cccd,diachi)
-values (N'Nguyễn Nguyệt Nga','123456789',N'Hà Nội');
- 
- select * from khachhang;
-
- insert into thuebao(sothuebao,loai,ngaydangki,ten)
- values('123456789',N'Trả trước','02-12-12',N'Nguyễn Nguyệt Nga');
-
- select * from thuebao;
